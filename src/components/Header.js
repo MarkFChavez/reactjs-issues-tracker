1d040c300b10aa483504b756e122d91e017e5f7b
Mark Chavez <markchav3z@gmail.com>
2017-12-05 22:07:06 +0800

    add links

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,18 +6,13 @@ const Header = props => (
       {props.title} Issues
     </span>
 
-    {/* <div>
-      <small className='header--subtitle'>
-        Make <a href='#' className='header--self'>#javascript</a> better!
-        Check out issues on <a href='#' className='header--self'>#redux</a> and <a href='#' className='header--self'>#webpack</a>.
-      </small>
-    </div> */}
-
     <div>
       <small className='header--subtitle'>
-        Follow me on <a href='#' className='header--self--underlined'>twitter</a> &amp; {' '}
-        <a href='#' className='header--self--underlined'>github</a>. If you want to contribute, submit a pull request {' '}
-        <a href='#' className='header--self--underlined'>here</a>.
+        Follow me on <a href='https://twitter.com/markisundefined' className='header--self--underlined'>Twitter</a> &amp; {' '}
+        <a href='https://github.com/mrkjlchvz' className='header--self--underlined'>GitHub</a>.
+        <br />
+        If you want to contribute, you are encouraged to do so! {' '}
+        Start <a href='https://github.com/mrkjlchvz/reactjs-issues-tracker/pulls' className='header--self--underlined'>here</a>.
       </small>
     </div>
   </header>
