9184c70117729bec05d3d206142bbfe6db19688e
Mark Chavez <markchav3z@gmail.com>
2017-12-07 13:48:01 +0800

    link for labels [wip]

--- src/components/LabelList.js
+++ src/components/LabelList.js
@@ -4,7 +4,9 @@ import * as helpers from '../helpers'
 const LabelList = ({labels}) => (
   <div className='card-label--container'>
     {labels.map(label =>
-      <span className='card--label' key={`label-${label.id}`} style={{backgroundColor: `#${label.color}`, color: helpers.getContrastYIQ(label.color)}}> {label.name} </span>
+      <span className='card--label' key={`label-${label.id}`} style={{backgroundColor: `#${label.color}`, color: helpers.getContrastYIQ(label.color)}}>
+        <a href='#' className='label-link' style={{color: 'inherit'}}>{label.name}</a>
+      </span>
     )}
   </div>
 )
