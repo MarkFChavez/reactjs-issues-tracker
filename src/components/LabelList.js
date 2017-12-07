fd166504ad2b8a4d5105012677bbba7e44e554bd
Mark Chavez <markchav3z@gmail.com>
2017-12-07 14:05:25 +0800

    rollback labellist

--- src/components/LabelList.js
+++ src/components/LabelList.js
@@ -5,7 +5,7 @@ const LabelList = ({labels}) => (
   <div className='card-label--container'>
     {labels.map(label =>
       <span className='card--label' key={`label-${label.id}`} style={{backgroundColor: `#${label.color}`, color: helpers.getContrastYIQ(label.color)}}>
-        <a href='#' className='label-link' style={{color: 'inherit'}}>{label.name}</a>
+        {label.name}
       </span>
     )}
   </div>
